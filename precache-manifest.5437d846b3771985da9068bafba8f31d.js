7a02a408a8874a71298c1a718b756d48250a53df
Kiwied <anik-anton@yandex.ru>
2020-07-22 20:43:32 +0300

    Updates

---
+++ precache-manifest.5437d846b3771985da9068bafba8f31d.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5b08fbf1bd66f5774e7c3c321fde43",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eee650aae697be68c098",
+    "url": "./static/css/main.a634ba89.chunk.css"
+  },
+  {
+    "revision": "db512c3b8cec272ecf39",
+    "url": "./static/js/2.2f503836.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2f503836.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eee650aae697be68c098",
+    "url": "./static/js/main.46ba31af.chunk.js"
+  },
+  {
+    "revision": "a8ef13021948c1ee7e9c",
+    "url": "./static/js/runtime-main.a5163ae5.js"
+  },
+  {
+    "revision": "14a176339fc00af3ae93be979f1593cc",
+    "url": "./static/media/Inter-Black.14a17633.woff2"
+  },
+  {
+    "revision": "b90562e0cf2dcfedca0b93ad7fa447d8",
+    "url": "./static/media/Inter-Black.b90562e0.woff"
+  },
+  {
+    "revision": "95b8a98959d1af9ab432d7ffe295ef94",
+    "url": "./static/media/Inter-Medium.95b8a989.woff"
+  },
+  {
+    "revision": "c709803c3cab6f1116039e881ecf531a",
+    "url": "./static/media/Inter-Medium.c709803c.woff2"
+  },
+  {
+    "revision": "1e081edc16d92d42aeccec760174fbf4",
+    "url": "./static/media/Inter-Regular.1e081edc.woff2"
+  },
+  {
+    "revision": "3ae6a7d3890c33d857fc00bd2e4c4820",
+    "url": "./static/media/Inter-Regular.3ae6a7d3.woff"
+  },
+  {
+    "revision": "50cf486163827b9b62c764268a435184",
+    "url": "./static/media/button-add.50cf4861.svg"
+  },
+  {
+    "revision": "b9b65c7e7e563b8df5ac93cc01872a72",
+    "url": "./static/media/button-edit.b9b65c7e.svg"
+  },
+  {
+    "revision": "66807b14703a11264da9f5cfffafe946",
+    "url": "./static/media/delete-button.66807b14.svg"
+  },
+  {
+    "revision": "e7a8906a3b0887219bad0d62de4f7612",
+    "url": "./static/media/like.e7a8906a.svg"
+  },
+  {
+    "revision": "b12167400da3396be4b9b6e69ba27f5d",
+    "url": "./static/media/liked.b1216740.svg"
+  },
+  {
+    "revision": "e889e45f7898567d7ada789a8ffa0e63",
+    "url": "./static/media/logo.e889e45f.svg"
+  }
+]);
